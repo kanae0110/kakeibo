85105df73f4c84019cb232a204f33cf6301840d7
EC2 Default User <ec2-user@ip-172-31-33-217.us-east-2.compute.internal>
2020-09-22 14:02:21 +0000

    ログインViewまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "application#hello"
   resources :books
+  
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_02_080632) do
+ActiveRecord::Schema.define(version: 2020_09_21_132624) do
 
   create_table "books", force: :cascade do |t|
     t.integer "inout"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2020_09_02_080632) do
     t.integer "year"
     t.integer "month"
     t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
